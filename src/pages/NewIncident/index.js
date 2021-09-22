9a1076665ae6b22c78a79b8e67691666354afb8e
jonpool <joaopl_4@hotmail.com>
2021-09-22 13:09:31 -0400

    excelUpload

--- src/pages/NewIncident/index.js
+++ src/pages/NewIncident/index.js
@@ -1,44 +1,49 @@
 import React,{useState} from 'react';
 import {Link, useHistory} from 'react-router-dom'; 
 import{FiArrowLeft} from 'react-icons/fi';
-
 import api from '../../Services/api'
-
 import './style.css';
-
 import logoImage from '../../assets/Logo.png';
 
-export default function NewIncident(){
+export default function NewIncident()
+ {
   const[title, setTitle] = useState('');
   const[description, setDescription] = useState('');
   const[value, setValue ] = useState('');
 
+  const [selectedFile, setSelectedFile] = useState();
+	const [isFilePicked, setIsFilePicked] = useState(false);
+
   const ongId = localStorage.getItem('ongId');
 
   const history = useHistory();
-  async function handleNewIncident(e){
+  async function handleNewIncident(e)
+  {
       e.preventDefault();
-
       const data ={
         title,
         description,
         value,
       };
-
-
-
-      try{
-          await api.post('incidents',data,{
+    try{
+        await api.post('incidents',data,{
             headers:{
               Authorization: ongId,
             }
           })
-       history.push('/profile');
+        history.push('/profile');
       }catch{
         alert('Erro ao cadastrar caso, tente novamente')
       }
+  } 
 
-  }  
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsFilePicked(true);
+    console.log(Event.target.files)
+  };
+  const handleSubmission = () => {
+  };
 
 
 
@@ -47,7 +52,7 @@ export default function NewIncident(){
   <div className="new-incident-container">
   <div className="content">
       <section>
-      <img src={logoImage} alt="be the hero"/>
+      <img src={logoImage} alt="Obie Comfort Solutions" />
 
       <h1>Register a new Case</h1>
       <p>Sign up, get in to the plarform and help people to find someone in need of something</p>
@@ -58,6 +63,12 @@ export default function NewIncident(){
       </Link>
       </section>
         <form>
+        <div>
+			      <input type="file" name="file" onChange={changeHandler} />
+            <div>
+                <button onClick={handleSubmission}className="button" type="submit">Submit</button>
+            </div>
+        </div>
           <input 
               placeholder="Title"
               value={title}
@@ -80,7 +91,7 @@ export default function NewIncident(){
           <button onClick ={handleNewIncident} className="button" type="submit">Register</button>
         </form>
       
+    </div>
   </div>
-</div>
-);
+  );
 }
